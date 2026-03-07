8fe9a02beeac413402becf59d42a10d83376933f
Lakshay Singh <lakshaysingh433433@gmail.com>
2026-03-07 16:33:11 +0530

    fix render deployment

--- backend/src/lib/socket.js
+++ backend/src/lib/socket.js
@@ -7,16 +7,19 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:5173"],
+    origin: [
+      "http://localhost:5173",
+      "https://new-chat-app-txlk.onrender.com",
+    ],
+    credentials: true,
   },
 });
+
+const userSocketMap = {};
 
 export function getReceiverSocketId(userId) {
   return userSocketMap[userId];
 }
-
-// used to store online users
-const userSocketMap = {}; // {userId: socketId}
 
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
@@ -24,7 +27,6 @@ io.on("connection", (socket) => {
   const userId = socket.handshake.query.userId;
   if (userId) userSocketMap[userId] = socket.id;
 
-  // io.emit() is used to send events to all the connected clients
   io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
   socket.on("disconnect", () => {
